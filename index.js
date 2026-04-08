528005e25d376ba9297be934c3bd335faa8e2efe
unknown <diego.tristan.rb@gmail.com>
2026-04-07 21:44:05 -0600

    fix ruta chat

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Ruta base
 app.get("/", (req, res) => {
-  res.send("JARVIS ONLINE FUNCIONANDO");
+  res.send("JARVIS ONLINE");
+});
+
+// Ruta chat (PRUEBA)
+app.get("/chat", (req, res) => {
+  const mensaje = req.query.mensaje || "";
+
+  res.send("JARVIS responde: " + mensaje + ", señor Ramos");
 });
 
 app.listen(PORT, () => {
-  console.log("Servidor activo en puerto " + PORT);
+  console.log("Servidor corriendo en puerto " + PORT);
 });
